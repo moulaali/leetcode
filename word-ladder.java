5ac0eaacba472d7d593368f1e594e4a3db3a6ec8
moulaali <hi.moulaali@gmail.com>
2024-05-26 14:01:44 -0700

    Update word-ladder.java

--- word-ladder.java
+++ word-ladder.java
@@ -1,9 +1,6 @@
 import java.util.*;
 
 /*
-
-https://leetcode.com/problems/word-ladder/description/
-
 Example 1:
 
 Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
@@ -22,6 +19,16 @@ Approach :
 3/ if we want the path, we need to store the pointers to parent in hashmap as we add elements. 
 we can follow the pointers to get the path.
 4/ The above map can also be used for seen/visited list
+
+
+To find all the possible neighbors, we dont have to iterate over all words. instead of Preprocess
+to create a map of pattern to possible words. for example
+
+h*t -> hot, hit
+*og -> cog, log, bog 
+
+To create this, for each word, replace each char by * and put a entry in map of : pattern -> word_list
+
 */
 
 class Solution {
@@ -30,10 +37,38 @@ class Solution {
     String begin = "hit";
     String end = "cog";
 
+    
     bfs(words, begin, end);
   }
 
+  static List<String> getPatterns(String word) {
+    List<String> patterns = new ArrayList<>();
+
+    char[] chars = word.toCharArray();
+    for (int i = 0; i < chars.length; i++) {
+      chars[i] = '*';
+      patterns.add(new String(chars));
+      chars[i] = word.charAt(i);
+    }
+
+    return patterns;
+  }
+
   private static void bfs(List<String> words, String begin, String end) {
+    
+    // Preprocess to create a pattern->[words]
+    Map<String, List<String>> patternToWordsMap = new HashMap<>();
+    for (String w : words) {
+      List<String> patterns = getPatterns(w);
+      for (String p : patterns) {
+        if (patternToWordsMap.get(p) == null) {
+          patternToWordsMap.put(p, new ArrayList<>());
+        }  
+        patternToWordsMap.get(p).add(w);
+      }
+    }
+    System.out.println("Created patternmap : " + patternToWordsMap);
+
     // BFS queue
     Queue<String> q = new LinkedList<>();
     q.add(begin);
@@ -63,35 +98,24 @@ class Solution {
       }
 
       // Enequeue unseen neighbors
-      for (String word : words) {
-        if (path.containsKey(word)) {
+      // Neighbors can be easily found via looking up possible patterns of this word
+      List<String> patterns = getPatterns(w);
+      for (String p : patterns) {
+        List<String> neighbors = patternToWordsMap.get(p);
+        if (neighbors == null) {
           continue;
         }
 
-        if (differByChar(w, word)) {
-          path.put(word, w);
-          q.add(word);
-        }
+        for (String n : neighbors) {
+          if (path.containsKey(n)) {
+          continue;
+          }
+
+          path.put(n, w);
+          q.add(n);
+        }        
       }
-    }
-  }
 
-  private static boolean differByChar(String s1, String s2) {
-    
-    if (s1.length() != s2.length()) {
-      return false;
     }
-
-    int diffCount = 0;
-    for (int i = 0; i < s1.length(); i++) {
-      if (s1.charAt(i) != s2.charAt(i)) {
-        diffCount++;
-        if (diffCount > 1) {
-          break;
-        }
-      }
-    }
-
-    return (diffCount == 1);
   }
 }
